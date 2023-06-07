ac3db966b111d3ecf4f0a1acaf0479d8cd72f79f
yungestAmine <purpleamine@gmail.com>
2023-06-07 16:15:16 +0200

    Finito stile frm Main

--- BattagliaNavale/BattagliaNavale/Form1.Designer.cs
+++ BattagliaNavale/BattagliaNavale/Form1.Designer.cs
@@ -34,9 +34,10 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(32)))), ((int)(((byte)(32)))));
             this.ClientSize = new System.Drawing.Size(1098, 635);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Battaglia Navale";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
